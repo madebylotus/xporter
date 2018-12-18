c7a1b629bbf3a76d4a97f5b91042e74bdd74e0ab
Matt Millsaps-Brewer <matt@madebylotus.com>
2018-12-18 06:52:57 -0500

    Use ActiveSupport::Concern

--- lib/xporter.rb
+++ lib/xporter.rb
@@ -2,6 +2,7 @@ require "csv"
 require "active_support/core_ext/class/attribute"
 require "active_support/core_ext/object/blank"
 require "active_support/core_ext/string/inflections"
+require "active_support/concern"
 
 begin
   require 'byebug'
